9dc568cb4410e3da92478ebe5b625d72fc946882
hyeonseongkang <gt9697@naver.com>
2021-08-26 17:44:47 +0900

    setting

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-import pygame
+import pygame, sys, random
+from time import sleep
+
+
+screenWidth = 360
+screenHeight = 480
+enemySpeed = 3
+coin = 3
+score = 0
+stage = 1          
+stageCount = 0
+
+enemyList = [ 'images/alien0.png', 'images/alien1.png', 'images/alien2.png', 'images/rocket1.png', 'images/rocket2.png', ]
